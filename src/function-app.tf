1c547853b33863ec5525b1d2797afb996bb95404
rlightley88 <rlightley88@gmail.com>
2023-09-21 20:43:26 +0100

    update

--- src/function-app.tf
+++ src/function-app.tf
@@ -1,3 +1,21 @@
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "law-${local.resource_suffix}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "ai" {
+  name                = "ai-${local.resource_suffix}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  workspace_id        = azurerm_log_analytics_workspace.law.id
+  application_type    = "web"
+
+  tags = local.default_tags
+}
+
 resource "azurerm_service_plan" "asp" {
   name                = "asp-${local.resource_suffix}"
   resource_group_name = azurerm_resource_group.rg.name
@@ -17,7 +35,18 @@ resource "azurerm_windows_function_app" "fa" {
   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
   service_plan_id            = azurerm_service_plan.asp.id
 
-  site_config {}
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY"             = data.azurerm_application_insights.ai.instrumentation_key
+    "APPLICATIONINSIGHTS_CONNECTION_STRING"      = data.azurerm_application_insights.ai.connection_string
+    "ASPNETCORE_ENVIRONMENT"                     = var.environment
+    "ApplicationInsightsAgent_EXTENSION_VERSION" = "~2"
+  }
+
+  connection_string {
+    name  = "AppConfig"
+    type  = "Custom"
+    value = data.azurerm_app_configuration.appconf.primary_read_key.0.connection_string
+  }
 
   tags = local.default_tags
 
